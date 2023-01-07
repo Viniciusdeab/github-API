0f56512c5e04c696e4d2653cc53c1151ed2753d2
Vinicius Brito <viniciusdeab@outlook.com>
2023-01-07 12:43:31 -0300

    Movendo o serviço de buscar o usuaria para o arquivo user na pasta service

--- src/script/index.js
+++ src/script/index.js
@@ -16,12 +16,6 @@ document.getElementById('input-search').addEventListener('keyup', (e)=>{
     }
 });
 
-
-async function user(userName){
-    const response = await fetch(`${baseUrl}/${userName}`);
-    return await response.json();
-}
-
 async function repos(userName){
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${repositoriesQuantity}`);
     return await response.json();
