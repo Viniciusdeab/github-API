5b1920dec4bfe05a8892f090405d44a2421dfadf
Vinicius Brito <viniciusdeab@outlook.com>
2023-01-07 22:16:18 -0300

    Ajuste no caminho da importação das variação

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
-import { user } from "./services/user.js";
-import { repositories } from "./services/repositories";
+import { user } from "src/script/services/user.js";
+import { repositories } from "src/script/services/user.js";
 
 document.getElementById('btn-search').addEventListener('click', ()=>{
     const userName = document.getElementById('input-search').value;
